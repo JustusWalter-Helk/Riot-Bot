06afbcccb2d23ce3d8a4caa8724086778770d3ec
Justus Walter-Helk <75504335+JustusWalter-Helk@users.noreply.github.com>
2023-12-01 18:43:11 +0100

    Removed tokens

--- src/start.ts
+++ src/start.ts
@@ -1,9 +1,11 @@
 import {Bot} from "./Bot"
 import { RiotAPI } from "./helpers/RiotAPI";
 import {Commands} from "./helpers/Commands";
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-const clientId = "912782868466528317"
-const botToken = "OTEyNzgyODY4NDY2NTI4MzE3.GtWoxq._sC14IG8yyOTgqayUrUNQL2eOWQdU3YvaEQ70U"
+const clientId = process.env.CLIENT_ID
+const botToken = process.env.BOT_TOKEN
 
 if(clientId == null) {console.log("ClientId in config file is undefined!"); process.exit();}
 if(botToken == null) {console.log("BotToken in config file is undefined!"); process.exit()}
